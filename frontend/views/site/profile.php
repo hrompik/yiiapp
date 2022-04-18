98e982a94e67f30400f03412ca59c7160f0d2cfd
Vitaliy Pikalov <hrompi@ya.ru>
2022-04-18 19:18:04 +0300

    upp

--- frontend/views/site/profile.php
+++ frontend/views/site/profile.php
@@ -14,8 +14,8 @@ $this->params['breadcrumbs'][] = $this->title;
     Твой Api Token, для его изменения покормите разработчика
     <p> <b><?=$model->auth_key ?></b> </p>
     Можно через ссылку:
-    <p><?=Html::a("back/pepyakas?access-token={$model->auth_key}","http://back/pepyakas?access-token={$model->auth_key}")?></p>
+    <p><?=Html::a("back.local/pepyakas?access-token={$model->auth_key}","http://back.local/pepyakas?access-token={$model->auth_key}")?></p>
     Либо HTTP Bearer Tokens:
-    <p>curl -i -H -head "Accept:application/json" -H "Authorization: Bearer <?=$model->auth_key ?>" "http://back/pepyakas"</p>
+    <p>curl -i -H -head "Accept:application/json" -H "Authorization: Bearer <?=$model->auth_key ?>" "http://back.local/pepyakas"</p>
 
 </div>
